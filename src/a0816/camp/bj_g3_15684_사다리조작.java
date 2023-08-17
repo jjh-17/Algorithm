2f663ec1fc3a9f537d27f646d7ef186cc74a4a47
jjo311 <jjho311@naver.com>
2023-08-17 10:38:09 +0900

    0816

--- src/a0816/camp/bj_g3_15684_사다리조작.java
+++ src/a0816/camp/bj_g3_15684_사다리조작.java
@@ -7,8 +7,8 @@ import java.io.*;
 public class bj_g3_15684_사다리조작 {
 
 	static final StringBuilder sb = new StringBuilder();
-	static int N, M, H, CNT, 
-			   LIM[], RESULT[], MAP[][];
+	static int N, M, H, CNT, MIN,
+			   MAP[][];
 	static boolean flag = false;
 	static final List<int[]> LINE_CAND = new ArrayList<>(); //가로선 후보군 가로출 i에서 세로줄 j~j+1
 	
@@ -21,73 +21,60 @@ public class bj_g3_15684_사다리조작 {
 		//입력
 		st = new StringTokenizer(br.readLine());
 		N=Integer.parseInt(st.nextToken()); M=Integer.parseInt(st.nextToken()); H=Integer.parseInt(st.nextToken());
-		LIM = new int[N]; //세로줄 당 연결된 가로선 개수
-		RESULT = new int[N]; //i번 세로선의 결과
-		MAP = new int[M][N]; //0은 빈칸, 1은 가로선
-		for(int i=0;i<M;i++) {
+		MIN = Integer.MAX_VALUE; MAP = new int[H+1][N+1]; //0은 빈칸, 1은 가로선
+		for(int i=1;i<=M;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-			LIM[b-1]+=1; LIM[b]+=1;
-			MAP[a-1][b-1]=1; MAP[a-1][b]=1;
+			MAP[a][b]=b+1; MAP[a][b+1]=b;
 		}
 		
-		//가로선 후보군 추출
-		for(int i=0;i<M;i++) {
-			for(int j=0;j<N;j++) {
-				if(MAP[i][j]==0) LINE_CAND.add(new int[] {i, j});
+		//가로선 후보군 추출: i가로줄의 j~j+1 세로줄
+		for(int i=1;i<=H;i++) {
+			for(int j=1;j<=N-1;j++) {
+				if(MAP[i][j]==0 && MAP[i][j+1]==0) LINE_CAND.add(new int[] {i, j});
 			}
 		}
 		
-		
-		for(int[] M : MAP) System.out.println(Arrays.toString(M));
-		System.out.println();
-		System.out.println(Arrays.toString(LIM));
-		System.out.println();
-		setResult();
-		System.out.println(Arrays.toString(RESULT));
-		
-		
-		
-		
+		dfs(0, 0);		
 		
 		br.close();
-		System.out.println(sb.toString());
+		System.out.println(MIN==Integer.MAX_VALUE ? -1 : MIN);
 	}
 	
 	
 	//가로줄 a에서 
-	static void dfs(int a, int b, int cnt) {
+	static void dfs(int idx, int cnt) {
+		if(flag) return;
+		
+		if(checkResult()) {
+			MIN = Integer.min(MIN, cnt);
+			if(cnt==0) flag = true;
+			return;
+		}
+		
+		if(cnt==3) return;
+		
+		for(int i=idx;i<LINE_CAND.size();i++) {
+			int[] cor = LINE_CAND.get(i);
+			if(MAP[cor[0]][cor[1]]==0 && MAP[cor[0]][cor[1]+1]==0) {
+				MAP[cor[0]][cor[1]]=cor[1]+1; MAP[cor[0]][cor[1]+1]=cor[1];
+				dfs(idx+1, cnt+1);
+				MAP[cor[0]][cor[1]]=0; MAP[cor[0]][cor[1]+1]=0;
+			}
+		}
 		
 	}
 	
+	
 	//Result 적용
-	static void setResult() {
-		for(int d=0;d<N;d++) {
-			System.out.println(d + " " + Arrays.toString(RESULT));
-			int i=0, j=d;
-			while(true) {
-				if(i==M) {
-					RESULT[j]+=1;
-					break;
-				}
-				if(MAP[i][j]==0) ++i; //가로선 연결X
-				else { //가로선 연결 존재
-					if(0<=j-1 && MAP[i][j-1]==1) { //좌측 이동
-						while(true) {
-							--j;
-							if(j==0 || (j>=0 && MAP[i][j]==0)) break;
-						}
-					} 
-//					else { //우측 이동
-//						while(true) {
-//							++j;
-//							if(j==N-1 || (j<N-1 && MAP[i][j]==0)) break;
-//						}
-//					}
-					++i;
-				}
-			}
+	static boolean checkResult() {
+		for(int d=1;d<=N;d++) {
+			int j=d;
+			for(int i=1;i<=H;i++) j = MAP[i][j]!=0 ? MAP[i][j] : j;
+			if(j!=d) return false;
 		}
+		
+		return true;
 	}
 
 }
