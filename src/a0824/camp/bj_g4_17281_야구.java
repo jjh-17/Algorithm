9a435f1a109b6a38e7b819cac8d1d908058bcefb
jjo311 <jjho311@naver.com>
2023-08-25 08:31:54 +0900

    0824

---
+++ src/a0824/camp/bj_g4_17281_야구.java
@@ -0,0 +1,26 @@
+package a0824.camp;
+
+import java.util.*;
+import java.io.*;
+
+public class bj_g4_17281_야구 {
+
+	static final StringBuilder sb = new StringBuilder();
+	static int N;
+	
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.setIn(new FileInputStream("res\\input_bj_g4_17281_야구.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		
+		
+		
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
