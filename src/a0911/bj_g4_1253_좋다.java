07d91358c5e9793d43a0b93524d62bd7e81d0be7
jjo311 <jjho311@naver.com>
2023-09-12 17:57:16 +0900

    0912

--- src/a0911/bj_g4_1253_좋다.java
+++ src/a0911/bj_g4_1253_좋다.java
@@ -28,36 +28,72 @@ public class bj_g4_1253_좋다 {
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
 		
-		
 		//
 		long cur_max, next_max;
 		int cur_max_start_idx=N, next_max_start_idx;
 		int cur_max_end_idx, next_max_end_idx;
 		int ANS=0;
 		
+//		//양수
+//		while(true) {
+//			cur_max_end_idx = cur_max_start_idx-1;
+//			cur_max = arr[cur_max_end_idx];
+//			cur_max_start_idx = getFirstTargetIdx(0, cur_max_end_idx, cur_max);
+//			if(cur_max_start_idx==0 || cur_max<0) break;
+//			
+//			next_max_start_idx = cur_max_start_idx;
+//			while(true) {
+//				next_max_end_idx = next_max_start_idx-1;
+//				next_max = arr[next_max_end_idx];
+//				next_max_start_idx = getFirstTargetIdx(0, next_max_end_idx, next_max);	
+//
+//				//다음 큰 수 2개로도 cur_max를 만들지 못하면 넘어간다.
+//				if(next_max*2<cur_max || next_max_start_idx==0) break;
+//		
+//				//다음 큰 수가 2개 이상이며, 그 2배가 현재 큰 수 인 경우
+//				if((next_max*2==cur_max && next_max_end_idx-next_max_start_idx>=1) 
+//						|| getFirstTargetIdx(0, next_max_end_idx, cur_max-next_max)<next_max_end_idx) {
+//					ANS += (cur_max_end_idx - cur_max_start_idx+1);
+//					break;
+//				}
+//			}
+//		}	
+		
+		
+//		System.out.println(getLastTargetIdx(2, 4, 10));
+		
+		//음수
+		long cur_min, next_min;
+		int cur_min_start_idx, next_min_start_idx;
+		int cur_min_end_idx=-1, next_min_end_idx;
+		
 		while(true) {
-			if(cur_max_start_idx==0) break;
-			cur_max_end_idx = cur_max_start_idx-1;
-			cur_max = arr[cur_max_end_idx];
-			cur_max_start_idx = getFirstIdx(0, cur_max_end_idx+1, cur_max);
+			cur_min_start_idx = cur_min_end_idx+1;
+			cur_min = arr[cur_min_start_idx];
+			cur_min_end_idx = getLastTargetIdx(cur_min_start_idx, N-1, cur_min);
+			if(cur_min_end_idx==N-1 || cur_min>=0) break;
 			
-			next_max_start_idx = cur_max_start_idx;
+			next_min_end_idx = cur_min_end_idx;
 			while(true) {
-				if(next_max_start_idx==0) break;
-				next_max_end_idx = next_max_start_idx-1;
-				next_max = arr[next_max_end_idx];
-				next_max_start_idx = Arrays.binarySearch(arr, next_max);
+				next_min_start_idx = next_min_end_idx+1;
+				next_min = arr[next_min_start_idx];
+				next_min_end_idx = getLastTargetIdx(next_min_start_idx, N-1, next_min);
 				
-				if(next_max*2<cur_max) break;
+				//다음 작은 수 2개로도 cur_min을 만들지 못하면 넘어간다.
+				if(next_min*2>cur_min || next_min_end_idx==0) break;
 				
 				//다음 큰 수가 2개 이상이며, 그 2배가 현재 큰 수 인 경우
-				if((next_max*2==cur_max && next_max_end_idx-next_max_start_idx>=1) 
-						|| getFirstIdx(0, next_max_end_idx+1, next_max)!=-1) {
-					ANS += (cur_max_end_idx - cur_max_start_idx + 1);
+				if((next_min*2==cur_min && next_min_end_idx-next_min_start_idx>=1) 
+						//여기가 문젠데 말이지
+						|| getLastTargetIdx(next_min_start_idx, N-1, next_min-cur_min)<=next_min_end_idx) {
+					System.out.println(cur_min_start_idx + " " + cur_min_end_idx + " " + next_min_start_idx + " " 
+										+ next_min_end_idx + " " + next_min);
+					ANS += (cur_min_end_idx - cur_min_start_idx+1);
 					break;
 				}
 			}
 		}	
+		
 	
 		//출력
 		sb.append(ANS);
@@ -66,15 +102,27 @@ public class bj_g4_1253_좋다 {
 	}
 	
 	// 중복 배열 내 target index 최솟값 반환
-	static int getFirstIdx(int start, int end, long target) {
-		int idxL=0, idxMid=(start+end)/2, idxR=end;
+	static int getFirstTargetIdx(int start, int end, long target) {
+		int mid;
 		
-		while(idxL<=idxR) {
-			idxMid = (idxL+idxR)/2;
-			if(arr[idxMid]<target) idxL=idxMid+1;
-			else end=idxMid-1;
+		while(start<=end) {
+			mid = (start+end)/2;
+			if(arr[mid]<target) start=mid+1;
+			else end=mid-1;
 		}
 
-		return idxL;
+		return start;
+	}
+	
+	// 중복 배열 내 target index 최댓값 반환
+	static int getLastTargetIdx(int start, int end, long target) {
+		int mid;
+		
+		while(start<=end) {
+			mid = (start+end)/2;
+			if(arr[mid]<=target) start=mid+1;
+			else end=mid-1;
+		}
+		return end;
 	}
 }
