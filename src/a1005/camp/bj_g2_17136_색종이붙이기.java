1fad172ac3d62249c3c77d1266622e48f7cad575
jjho311 <jjho311@naver.com>
2023-10-05 21:49:35 +0900

    1005

--- src/a1005/camp/bj_g2_17136_색종이붙이기.java
+++ src/a1005/camp/bj_g2_17136_색종이붙이기.java
@@ -16,7 +16,6 @@ public class bj_g2_17136_색종이붙이기 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		
 //		입력
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
@@ -24,8 +23,7 @@ public class bj_g2_17136_색종이붙이기 {
 		}
 		
 //		알고리즘
-		
-		
+		dfs(0, 0, 0);
 		
 //		출력
 		sb.append(ANS==Integer.MAX_VALUE ? -1 : ANS);
@@ -35,31 +33,58 @@ public class bj_g2_17136_색종이붙이기 {
 	
 //	현재 위치, 색종이 사용 횟수
 	static void dfs(int i, int j, int cnt) {
+//		탐색 종료
 		if(i==N) {
 			ANS = Integer.min(ANS, cnt);
 			return;
 		}
 		
-//		현 위치를 좌상단 꼭지점으로 두었을 때, 색종이를 붙일 수 있으면 붙인다.
-		if(map[i][j]==1) {
-			for(int s=0;s<5;s++) {
-//				색종이 s가 더 이상 남아있지 않거나 필드를 벗어나면 다음 색종이 탐색
-				if(rest[s]==0 || i+size[s]-1>=N || j+size[s]-1>=N) continue;
+		if(ANS < cnt) return;
+		
+//		다음 인덱스로 이동
+		if(map[i][j]==0) {
+			int nj = j+1==N ? 0 : j+1;
+			int ni = nj==0 ? i+1 : i;
+			dfs(ni, nj, cnt);
+			return;
+		}
+		
+//		 백업
+		int back[][] = new int[N][N];
+		for(int a=0;a<N;a++) {
+			for(int b=0;b<N;b++) back[a][b] = map[a][b];
+		}
+			
+		for(int s=0;s<5;s++) {
+//			색종이 s가 더 이상 남아있지 않거나 필드를 벗어나면 다음 색종이 탐색
+			if(rest[s]==0 || i+size[s]-1>=N || j+size[s]-1>=N) continue;
+			
+			if(check(i, j, size[s])) {
+				--rest[s];
 				
+				int nj = j+size[s]==N ? 0 : j+size[s];
+				int ni = nj==0 ? i+1 : i;
+				dfs(ni, nj, cnt+1);
 				
+//				복원
+				++rest[s];
+				for(int a=0;a<N;a++) {
+					for(int b=0;b<N;b++) map[a][b] = back[a][b];
+				}
+			}
+				
+		}
+	}
+
+//	색종이를 덮을 수 있는지 판단
+	static boolean check(int i, int j, int size) {
+		for(int a=i;a<i+size;a++) {
+			for(int b=j;b<j+size;b++) {
+				if(map[a][b]==1) map[a][b]=0;
+				else return false;
 			}
 		}
 		
-		
-//		다음 인덱스로 이동
-		int nj = j+1==N ? 0 : j+1;
-		int ni = nj==0 ? i+1 : i;
-		dfs(ni, nj, cnt);
-	}
-
-	static int[] getNewLoc(int i, int j, int di, int dj) {
-		
-		
-		
+		return true;
 	}
 }
