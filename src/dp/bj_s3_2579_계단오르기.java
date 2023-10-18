590656456ecb0231692164349085b00c45c7e756
jjho311 <jjho311@naver.com>
2023-10-18 21:41:58 +0900

    1018

--- src/dp/bj_s3_2579_계단오르기.java
+++ src/dp/bj_s3_2579_계단오르기.java
@@ -3,10 +3,6 @@ package dp;
 import java.util.*;
 import java.io.*;
 
-//계단은 한 번에 한 계단씩 또는 두 계단씩 오를 수 있다. 즉, 한 계단을 밟으면서 이어서 다음 계단이나, 다음 다음 계단으로 오를 수 있다.
-//연속된 세 개의 계단을 모두 밟아서는 안 된다. 단, 시작점은 계단에 포함되지 않는다.
-//마지막 도착 계단은 반드시 밟아야 한다.
-
 public class bj_s3_2579_계단오르기 {
 
 	static final StringBuilder sb = new StringBuilder();
@@ -18,27 +14,17 @@ public class bj_s3_2579_계단오르기 {
 		
 //		입력
 		int N = Integer.parseInt(br.readLine());
-		int[][] dp = new int[N+1][2];
-		for(int i=1;i<=N;i++) {
-			int score = Integer.parseInt(br.readLine());
-			
-//			1칸 이동
-			if(i-1>=0 && dp[i][0]<dp[i-1][0]+score && dp[i-1][1]<2) {
-				dp[i][0] = dp[i-1][0] + score;
-				dp[i][1] = dp[i-1][1] + 1;
-			}
-			
-//			2칸 이동
-			if(i-2>=0 && dp[i][0]<=dp[i-2][0]+score) {
-				dp[i][0] = dp[i-2][0] + score;
-				dp[i][1] = 1;
-			}
-			
-//			for(int j=0;j<=N;j++) System.out.print(dp[j][0] + " ");
-//			System.out.println();
-		}
+		int[] dp = new int[N+1];
+		int[] scores = new int[N+1];
+
+		for(int i=1;i<=N;i++) scores[i] = Integer.parseInt(br.readLine());
 		
-		sb.append(dp[N][0]);
+//		DP
+		if(N>=1) dp[1] = scores[1];
+		if(N>=2) dp[2] = dp[1] + scores[2];
+		for(int i=3;i<=N;i++) dp[i] = Integer.max(dp[i-3] + scores[i-1], dp[i-2]) + scores[i];
+		
+		sb.append(dp[N]);
 		System.out.println(sb.toString());
 		br.close();
 	}
