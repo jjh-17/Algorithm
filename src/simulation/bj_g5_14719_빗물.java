66db4d47859b78050a7a75c876bc39efa3ac7217
jjho311 <jjho311@naver.com>
2024-02-28 12:11:54 +0900

    2024/02/28 Simulation

--- src/simulation/bj_g5_14719_빗물.java
+++ src/simulation/bj_g5_14719_빗물.java
@@ -8,7 +8,6 @@ public class bj_g5_14719_빗물 {
 	
 	static int H, W, ans;
 	static int[] arr;
-	static final List<Integer> list = new ArrayList<>(); // 증가하다가 감소하는 idx 저장
 	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res\\input_bj_g5_14719_빗물.txt"));
@@ -23,26 +22,28 @@ public class bj_g5_14719_빗물 {
 		
 		st = new StringTokenizer(br.readLine());
 		arr = new int[W];
-		for(int i=0;i<W;i++)
+		for(int i=0;i<W;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		
 //		알고리즘
-//		1) W<=2 이면 빗물을 담을 수 없음
-		if(W<=2) {
-			System.out.println(ans);
-			br.close();
-		}
-		
-//		2) 높이 배열 순회
-		for(int i=0;i<W;i++) {
+		int L=0, R=0, minH;
+		for(int i=1;i<W-1;i++) {
+			L=0; 
+			R=0;
 			
+			for(int l=0;l<i;l++)
+				L = Math.max(L, arr[l]);
+			for(int l=i+1;l<W;l++)
+				R = Math.max(R, arr[l]);
+			minH = Math.min(L, R);
+			
+			if(arr[i] < minH)
+				ans += (minH-arr[i]);
 		}
-
-		
 		
 //		출력
 		System.out.println(ans);
 		br.close();
 	}
-
 }
