285eea4713b11a2fb206eb3870365d9c20a258bb
jjh <jjho311@naver.com>
2024-06-17 23:30:22 +0900

    Feat: 3차원 visited 탐색 리팩토링

--- src/graph/bj_g3_2206_벽부수고이동하기.java
+++ src/graph/bj_g3_2206_벽부수고이동하기.java
@@ -4,18 +4,19 @@ import java.util.*;
 import java.io.*;
 
 public class bj_g3_2206_벽부수고이동하기 {
-
-	static final StringBuilder sb = new StringBuilder();
-	static final int[] di = {-1, 0, 1, 0}, dj = {0, 1, 0, -1};
-	static int N, M;
-	static int[][] map;
-	static boolean[][][] v;
 	
-	static class Info{
-		int i, j, cntW, cntN;
-		public Info(int i, int j, int cntW, int cntN) {
-			this.i=i; this.j=j; 
-			this.cntW=cntW; this.cntN = cntN;
+	static final int D = 4;
+	static final int[] di = {-1, 0, 1, 0};
+	static final int[] dj = {0, 1, 0, -1};
+	static int N, M, ans;
+	static char[][] map;
+	static class Info {
+		int i, j, L, cnt_break;	// 좌표, 이동 거리, 벽 부수기 가능 여부
+		
+		Info(int i, int j, int L, int cnt_break) {
+			this.i = i; this.j = j; 
+			this.L = L;
+			this.cnt_break = cnt_break;
 		}
 	}
 	
@@ -24,73 +25,68 @@ public class bj_g3_2206_벽부수고이동하기 {
 		System.setIn(new FileInputStream("res\\input_bj_g3_2206_벽부수고이동하기.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		
-		//입력
+	
+//		입력
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		v = new boolean[N][M][2]; //각 좌표애 벽을 부순 상태로 
-		map = new int[N][M];
-		for(int i=0;i<N;i++) {
+		map = new char[N+1][M+1];
+		for(int i=1;i<=N;i++) {
 			String str = br.readLine();
-			for(int j=0;j<M;j++) map[i][j] = str.charAt(j)-'0';
+			for(int j=0;j<M;j++)	map[i][j+1] = str.charAt(j);
 		}
 		
-		//알고리즘
-		bfs();
+//		알고리즘
+		solution();
 		
-		
-		//출력
-		System.out.println(sb.toString());
+//		출력
+		System.out.println(ans);
 		br.close();
 	}
 	
-	static void bfs() {
-		final ArrayDeque<Info> queue = new ArrayDeque<>();
+	static void solution() {
+		final ArrayDeque<Info> dq = new ArrayDeque<>();
+		final boolean[][][] v = new boolean[2][N+1][M+1];
 		
-		//1*1 판정
+//		1 * 1인 경우
 		if(N==1 && M==1) {
-			sb.append(1);
+			ans = 1;
 			return;
 		}
-
-		v[0][0][0] = true;
-		queue.offerLast(new Info(0, 0, 0, 1));
 		
-		while(!queue.isEmpty()) {
-			Info cur = queue.pollFirst();
+		dq.offerLast(new Info(1, 1, 1, 0));
+		v[0][1][1] = true;
+		ans = -1;
+		while(!dq.isEmpty()) {
+			Info info = dq.pollFirst();
 			
-			//벽을 부수지 않고 이동
-			for(int d=0;d<4;d++) {
-				int ni = cur.i + di[d];
-				int nj = cur.j + dj[d];
+			for(int d=0;d<D;d++) {
+				int ni = info.i + di[d];
+				int nj = info.j + dj[d];
 				
-				//신규 좌표가 필드를 벗어나면 넘어감
-				if(ni<0 || ni>=N || nj<0 || nj>=M) continue;
-				
-				//도착지에 도달
-				if(ni==N-1 && nj==M-1) {
-					sb.append(cur.cntW + cur.cntN + 1);
+//				도착 여부 체크
+				if(ni == N && nj == M) {
+					ans = info.L + 1;
 					return;
 				}
 				
-				//벽을 부수지 않고 이동
-				//해당 좌표에 들른 적이 없으며, 장애물이 없다면
-				if(!v[ni][nj][cur.cntW] && map[ni][nj]==0) {
-					v[ni][nj][cur.cntW] = true;
-					queue.offerLast(new Info(ni, nj, cur.cntW, cur.cntN+1));
+//				맵 벗어남 여부 체크
+				if(ni < 1 || ni > N || nj < 1 || nj > M)	continue;
+				
+//				이동 위치가 벽이며, 부수고 이동 가능한 경우
+				if(map[ni][nj] == '1' && info.cnt_break == 0 && !v[1][ni][nj]) {
+					v[1][ni][nj] = true;
+					dq.offerLast(new Info(ni, nj, info.L + 1, 1));
+					continue;
 				}
 				
-				//벽을 부수고 이동
-				//벽 부순 횟수가 0, 벽 부순 상태로 해당 좌표에 들른 적이 없으며, 해당 좌표에 벽이 있다면
-				if(cur.cntW==0 && !v[ni][nj][1] && map[ni][nj]==1) {
-					v[ni][nj][1] = true;
-					queue.offerLast(new Info(ni, nj, 1, cur.cntN));
+//				이동 위치가 뚫려있는 경우
+				if(map[ni][nj] == '0' && !v[info.cnt_break][ni][nj]) {
+					v[info.cnt_break][ni][nj] = true;
+					dq.offerLast(new Info(ni, nj, info.L + 1, info.cnt_break));
 				}
-			}	
-		}	
-		sb.append(-1);
+			}
+		}
 	}
 }
